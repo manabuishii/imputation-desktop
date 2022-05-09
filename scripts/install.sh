5dddb8f3631e4bae61c3c908020f3564a7aa1742
Manabu ISHII <manabu.ishii.rb@gmail.com>
2022-05-09 10:37:42 +0900

    Add dockerfile (#1)
    * Add install.sh
    
    * Start each programs
    
    * Change install.sh directory
    
    * Add setting files
    
    * Write how to execute with container

---
+++ scripts/install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git build-essential libffi-dev libssl-dev libcurl4-openssl-dev zlib1g-dev -y --no-install-recommends
+# pip install cwltool==3.1.20210816212154
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
+sudo mkdir /usr/local/sapporo
+sudo chmod 777 /usr/local/sapporo
+cd /usr/local/sapporo
+
+curl -O -L https://nodejs.org/dist/v14.17.6/node-v14.17.6-linux-x64.tar.xz
+xz -d node-v14.17.6-linux-x64.tar.xz
+tar -xvf node-v14.17.6-linux-x64.tar
+# TODO install nodejs into PATH
+# export PATH=~/nodejs/node-v14.17.6-linux-x64/bin:$PATH
+# root@825c8ce40b58:~# ls -l /usr/local/sapporo/node-v14.17.6-linux-x64/bin/npm
+# lrwxrwxrwx 1 1001 1001 38 Aug 30  2021 /usr/local/sapporo/node-v14.17.6-linux-x64/bin/npm -> ../lib/node_modules/npm/bin/npm-cli.js
+# root@825c8ce40b58:~# npm
+# bash: npm: command not found
+# root@825c8ce40b58:~# ls -l /usr/bin/node
+# lrwxrwxrwx 1 root root 58 Apr 27 14:16 /usr/bin/node -> /usr/local/sapporo/nodejs/node-v14.17.6-linux-x64/bin/node
+# root@825c8ce40b58:~# ls -l /usr/local/sapporo/nodejs/node-v14.17.6-linux-x64/bin/node
+# ls: cannot access '/usr/local/sapporo/nodejs/node-v14.17.6-linux-x64/bin/node': No such file or directory
+# root@825c8ce40b58:~# 
+
+
+# ln -s /usr/local/sapporo/nodejs/node-v14.17.6-linux-x64/bin/node /usr/bin/node
+# ln -s /usr/local/sapporo/nodejs/node-v14.17.6-linux-x64/bin/npm /usr/bin/npm
+# ln -s /usr/local/sapporo/nodejs/node-v14.17.6-linux-x64/bin/npx /usr/bin/npx
+
+ln -s /usr/local/sapporo/node-v14.17.6-linux-x64/bin/node /usr/bin/node
+ln -s /usr/local/sapporo/node-v14.17.6-linux-x64/bin/npm /usr/bin/npm
+ln -s /usr/local/sapporo/node-v14.17.6-linux-x64/bin/npx /usr/bin/npx
+
+curl -O -L https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tgz
+tar zxvf Python-3.9.7.tgz
+cd Python-3.9.7
+./configure --prefix=/usr/local/sapporo/python/py397
+make
+make install
+
+cd ..
+
+# imputation server
+git clone https://github.com/manabuishii/imputationserver-web-ui.git
+cd imputationserver-web-ui
+/usr/local/sapporo/python/py397/bin/python3 -m venv venv-imputationserver-web-ui
+source venv-imputationserver-web-ui/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+nohup python3 app.py &
+deactivate
+cd ..
+
+# sapporo-service
+git clone https://github.com/sapporo-wes/sapporo-service.git -b 1.0.16
+cd sapporo-service
+## setting files
+cp /scripts/executable_workflows.json sapporo/executable_workflows.json
+cp /scripts/uwsgi.yaml uwsgi.yaml
+##
+/usr/local/sapporo/python/py397/bin/python3 -m venv venv-sapporo-service
+source venv-sapporo-service/bin/activate
+pip install sapporo==1.0.16
+pip install cwltool==3.1.20210816212154
+nohup python3 sapporo/app.py --host=0.0.0.0 --run-sh sapporo/run.sh  --executable-workflows sapporo/executable_workflows.json &
+deactivate
+cd ..
+
+
+
+
+# sapporo-web
+git clone https://github.com/sapporo-wes/sapporo-web.git -b 1.0.10
+cd sapporo-web
+curl -o src/plugins/loadPreRegisteredServices.ts https://raw.githubusercontent.com/sapporo-wes/sapporo-web/d7be4a559ba3d431aef05a1f93d11254ba8083b8/src/plugins/loadPreRegisteredServices.ts
+# copy setting files
+cp /scripts/nuxt.config.ts nuxt.config.ts
+cp /scripts/preRegisteredServices.json src/assets/preRegisteredServices.json
+# TODO set readonly mode
+npm install yarn
+# export PATH=node_modules/.bin:$PATH
+node_modules/.bin/yarn install --flozen-lock
+node_modules/.bin/yarn generate
+nohup node_modules/.bin/yarn start & 
+
+# 
+cd ..
+
---
+++ scripts/nuxt.config.ts
@@ -0,0 +1,70 @@
+import { NuxtConfig } from '@nuxt/types'
+import colors from 'vuetify/lib/util/colors'
+
+const config: NuxtConfig = {
+  ssr: false,
+  target: 'static',
+  head: {
+    title: 'sapporo-web',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'icon alternate', type: 'image/png', href: '/favicon.png' },
+    ],
+  },
+  loading: { color: '#fff' },
+  css: ['~/assets/common.css'],
+  plugins: [
+    '~/plugins/vuetifyColors.ts',
+    { src: '~/plugins/localStorage.ts', ssr: false },
+    '~/plugins/dayjs.ts',
+    '~/plugins/vue-clipboard2.ts',
+    '~/plugins/loadPreRegisteredServices.ts',
+  ],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+  modules: ['@nuxtjs/axios', 'nuxt-webfontloader'],
+  axios: {},
+  webfontloader: {
+    google: {
+      families: ['Quicksand', 'Fira Code'],
+    },
+  },
+  vuetify: {
+    theme: {
+      themes: {
+        light: {
+          primary: colors.indigo.darken4,
+          secondary: colors.teal.darken4,
+          info: colors.grey.darken4,
+          error: colors.red.darken4,
+        },
+      },
+    },
+  },
+  srcDir: './src/',
+  server: {
+    host: process.env.NUXT_HOST || '0.0.0.0',
+    port: process.env.NUXT_PORT || 1121,
+  },
+  router: {
+    base: process.env.SAPPORO_URL_PREFIX || '/',
+  },
+  generate: {
+    fallback: true,
+  },
+  env: {
+    SAPPORO_WEB_VERSION: process.env.npm_package_version || '',
+    LOAD_PRE_REGISTERED_SERVICES:
+      process.env.LOAD_PRE_REGISTERED_SERVICES || 'true',
+  },
+}
+
+export default config
