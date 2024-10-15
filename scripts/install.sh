594f4473418c850d946ea68bb056f1718e5df6ad
Manabu Ishii <manabu.ishii.rb@gmail.com>
2024-10-15 14:47:39 +0900

    Fix to set dot env file with variable name

--- scripts/install.sh
+++ scripts/install.sh
@@ -51,7 +51,8 @@ git clone https://github.com/manabuishii/imputation-desktop.git
 git clone https://github.com/ddbj/imputation-server-ui.git
 cd imputation-server-ui
 # create dot env for secret key
-tr -cd '[:alnum:][:punct:]' < /dev/urandom | tr -d '\\"`' | tr -d "'" |head -c 20 > .env
+SECRET_KEY=$(tr -cd '[:alnum:][:punct:]' < /dev/urandom | tr -d '\\"`' | tr -d "'" |head -c 20)
+echo "IMPUTATION_SERVER_SECRET_KEY=${SECRET_KEY}" > .env
 
 ${INSTALLDIR}/python/py397/bin/python3 -m venv venv-imputationserver-web-ui
 source venv-imputationserver-web-ui/bin/activate
