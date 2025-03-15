b83133d6ecd9c2ccc00b7767bceb7fcf284b601b
Manabu ISHII <manabu.ishii.rb@gmail.com>
2025-03-15 10:56:20 +0900

    Add pgscatalog workflow

--- scripts/install.sh
+++ scripts/install.sh
@@ -105,6 +105,7 @@ cd ..
 # 9c15e7a4676c1a2dd2cf089f8ebe1a137624e8b7
 BEAGLE_WORKFLOW_URL=$PWD/imputation-server-wf/Workflows/beagle-imputation-scatter-region.cwl
 HIBAG_WORKFLOW_URL=$PWD/imputation-server-wf/Workflows/hla-imputation/filterChrom-runhibag.cwl
+PGSCATALOG_WORKFLOW_URL=$PWD/imputation-server-ui/workflows/nextflow/pgs.nf
 
 if [ ! -f "${EXECUTABLEWORKFLOWSJSON_PATH}" ]; then
 cat <<EOF > ${EXECUTABLEWORKFLOWSJSON_PATH}
@@ -120,6 +121,13 @@ cat <<EOF > ${EXECUTABLEWORKFLOWSJSON_PATH}
       "workflow_name": "hibag",
       "workflow_url": "file://${HIBAG_WORKFLOW_URL}",
       "workflow_type": "CWL",
+      "workflow_type_version": "v1.0",
+      "workflow_attachment": []
+    },
+        {
+      "workflow_name": "pgs",
+      "workflow_url": "file://${PGSCATALOG_WORKFLOW_URL}",
+      "workflow_type": "Nextflow",
       "workflow_type_version": "v1.0",
       "workflow_attachment": []
     }
